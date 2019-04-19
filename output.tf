bd9175c6c05ab70e2858d469e8461e6d3d26fbde
murawakimitsuhiro <mitsuhiromurawaki@gmail.com>
2019-04-19 17:27:35 +0900

    Update output subnet list -> string

--- output.tf
+++ output.tf
@@ -1,5 +1,7 @@
 locals {
-  vpc_ids = "${aws_vpc.cluster_vpc.*.id}"
+  vpc_ids           = "${aws_vpc.cluster_vpc.*.id}"
+  public_subnet_1as = "${aws_subnet.public_subnet_ap-northeast-1a.*.id}"
+  public_subnet_1bs = "${aws_subnet.public_subnet_ap-northeast-1b.*.id}"
 }
 
 output "vpc_id" {
@@ -7,9 +9,9 @@ output "vpc_id" {
 }
 
 output "public_subnet_1a" {
-  value = "${aws_subnet.public_subnet_ap-northeast-1a.*.id}"
+  value = "${length(local.public_subnet_1as) == 0 ? "" : element(concat(local.public_subnet_1as, list("")), 0)}"
 }
 
 output "public_subnet_1b" {
-  value = "${aws_subnet.public_subnet_ap-northeast-1b.*.id}"
+  value = "${length(local.public_subnet_1bs) == 0 ? "" : element(concat(local.public_subnet_1bs, list("")), 0)}"
 }
